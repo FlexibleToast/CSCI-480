8a09af0b15e5d9f65113266ae1730ca49bf59966
Joseph McDade <JMcDade42@gmail.com>
2018-09-06 22:38:00 -0500

    Initialize assignment 2

---
+++ Z1757419_A2_dir/Assign2.cc
@@ -0,0 +1,19 @@
+/*********************************************************************
+   PROGRAM:    CSCI 480 Assignment 2
+   PROGRAMMER: Joseph McDade
+   LOGON ID:   z1757419
+   DUE DATE:   09/18/2018
+
+   FUNCTION:   This program adds practice of using pipe()
+*********************************************************************/
+
+#include <iostream>
+#include <unistd.h>     // fork,pipe
+#include <wait.h>       // wait
+#include <stdlib.h>     // system
+
+using namespace std;
+
+int main() {
+    return 0;
+}
