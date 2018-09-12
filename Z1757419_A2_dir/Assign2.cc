7c1564a42efd2886c4958be10f414247fbc5e970
Joseph McDade <JMcDade42@gmail.com>
2018-09-11 22:35:14 -0500

    WIP: Performs asked task, need to clean up code

--- Z1757419_A2_dir/Assign2.cc
+++ Z1757419_A2_dir/Assign2.cc
@@ -10,16 +10,21 @@
 *********************************************************************/
 
 #include <iostream>
+#include <iomanip>			// setw
 #include <unistd.h>     // fork,pipe
 #include <wait.h>       // wait
-#include <stdlib.h>     // system
-#include <string.h>			// atoi, itoa,
+#include <stdlib.h>     // system, itoa, atoi
+#include <string.h>			//
 
 using namespace std;
 
 // Prototyping statements
+void printValue(const char proc[], char value[]);
+void performMaths(char value[]);
 
-// Set global variables
+// Set global constants
+const int READ_FD = 0;
+const int WRITE_FD = 1;
 const int SIZE = 90;
 
 int main() {
@@ -28,7 +33,7 @@ int main() {
   // Create file descriptors
   int par_fd[2];
   int child_fd[2];
-	// int grand_fd[2];
+	int grand_fd[2];
   // pipeC = pipe(grand_fd);
   // Check if any pipe has an error
   if(pipe(par_fd) == -1){
@@ -39,6 +44,10 @@ int main() {
 		std::cout << "Second pip failed" << '\n';
 		exit(-5);
 	}
+	if(pipe(grand_fd) == -1){
+		std::cout << "Third pip failed" << '\n';
+		exit(-5);
+	}
 
   // Create child processes
   pid_t childPID = fork();
@@ -46,70 +55,96 @@ int main() {
   if(childPID == -1){
     std::cout << "Child process failed to start" << '\n';
     exit(-5);
-  } else if(childPID > 0)
-  { ////////// Parent process //////////
-    close(par_fd[0]); 	// Close pipeA read end
-    // close(grand_fd[1]);	// Close pipeC write end
-    // Call parent process
-		std::cout << "Test of the pipeA" << '\n';
-		const char *hello = "42 is the answer and the answer is: ";
-		std::cout << hello << '\n';
-		write(par_fd[1], hello, strlen(hello)+1);
+  }
+	else if(childPID > 0)
+  { /********* Parent process *********/
+    close(par_fd[READ_FD]); 		// Close pipeA read end
+    close(grand_fd[WRITE_FD]);	// Close pipeC write end
+
+		// Initialize values
+		const char *par = "Parent:";
+		char value[SIZE] = "1";
+		while (atoi(value) < 99999999)
+		{	//While value is still less than 99999999
+			printValue(par, value);
+			performMaths(value);
+			write(par_fd[WRITE_FD], value, strlen(value)+1);
+			read(grand_fd[READ_FD], value, SIZE);
+		}
+		write(par_fd[WRITE_FD], value, strlen(value)+1);
     wait(0);
+		close(par_fd[WRITE_FD]);
+		close(grand_fd[READ_FD]);
 		exit(0);
-  } else { // Child process
+  }
+	else { // Child process
     // Create grand child process
     pid_t grandPID = fork();
     //Check if error
     if(grandPID == -1){
       std::cout << "Grand child process failed to start" << '\n';
       exit(-5);
-    } else if(grandPID > 0)
-    { ////////// Child process //////////
-      close(par_fd[1]);		// Close pipeA write end
-			close(child_fd[0]);	// Close pipeB read end
-      // Call child function
-      // Write pipeB
-			char buffer[SIZE] = "1";
-			std::cout << "This is the child" << '\n';
-			read(par_fd[0], buffer, SIZE);
-			int ans = atoi(buffer);
-			std::cout << buffer << ans << '\n';
+    }
+		else if(grandPID > 0)
+    { /********* Child process *********/
+      close(par_fd[WRITE_FD]);	// Close pipeA write end
+			close(child_fd[READ_FD]);	// Close pipeB read end
+			const char *child = "Child:";
+			char buffer[SIZE] = "1";	// Prime the read
+			while (atoi(buffer) < 99999999)
+			{	//While buffer is still less than 99999999
+				read(par_fd[READ_FD], buffer, SIZE);
+				printValue(child, buffer);
+				performMaths(buffer);
+				write(child_fd[WRITE_FD], buffer, strlen(buffer)+1);
+			}
+			write(child_fd[WRITE_FD], buffer, strlen(buffer)+1);
       wait(0);
-    } else { ////////// Grand child process //////////
-			std::cout << "This is the grandChild" << '\n';
-      // Close pipeB write
-      // Close pipeC read
-      // Read pipeB
-      // Call grand child function
-      // Write pipeC
+			close(par_fd[READ_FD]);
+			close(child_fd[WRITE_FD]);
+			exit(0);
+    }
+		else { /********* Grand child process *********/
+			close(child_fd[WRITE_FD]);	// Close pipeB write end
+			close(grand_fd[READ_FD]);		// Close pipeC read end
+			const char *grand = "Grandchild:";
+			char buffer[SIZE] = "1";	// Prime the read
+			while (atoi(buffer) < 99999999)
+			{	//While buffer is still less than 99999999
+				read(child_fd[READ_FD], buffer, SIZE);
+				printValue(grand, buffer);
+				performMaths(buffer);
+				write(grand_fd[WRITE_FD], buffer, strlen(buffer)+1);
+			}
+			write(grand_fd[WRITE_FD], buffer, strlen(buffer)+1);
+      wait(0);
+			close(child_fd[READ_FD]);
+			close(grand_fd[WRITE_FD]);
+			exit(0);
     }
   }
   return 0;
 }
+/////////////////////////////////  Functions  //////////////////////////////////
+/*******************************************************************************
+Function: void printValue(const *char proc, const *char value)
+Use: Prints the calling process and the current value
+Arguments:	proc	- Calling process
+						value	- Current value
+Returns: Nothing
+*******************************************************************************/
+void printValue(const char proc[], char value[]) {
+	std::cout << setw(14) << left << proc << "Value: " << value << '\n';
+}
 
-/* Parent process
-Create char array value and buffer
-create int M
-while M is less than 99999999
-  cout value
-  perform maths()
-  write to pipeA
-  read pipeC
-  convert to M */
-
-/* Child function
-cout value
-perform maths()
-return value */
-
-/* Grand child function
-cout value
-perform maths()
-return value */
-
-/* Perform maths
-convert string to int
-M = 4 * M + 3;
-convert to string
-return string */
+/*******************************************************************************
+Function: const *char	performMaths(const *char value)
+Use: Performs 4 * current value + 3
+Arguments:	value	- Current value
+Returns: value after maths performed
+*******************************************************************************/
+void	performMaths(char value[]){
+	int newValue = atoi(value);
+	newValue = 4 * newValue + 3;
+	sprintf(value, "%d", newValue);
+}
