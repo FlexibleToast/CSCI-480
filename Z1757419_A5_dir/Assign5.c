c99643286754fc5d3ee8f3b04a63662b02a25832
Joseph McDade <JMcDade42@gmail.com>
2018-10-20 14:02:26 -0500

    Assign5: Test semaphore/pthread succeeds

--- Z1757419_A5_dir/Assign5.c
+++ Z1757419_A5_dir/Assign5.c
@@ -10,15 +10,45 @@
 *******************************************************************************/
 
 #include <stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
 
 // Prototyping statements
+void* test_run();
 
 // Set global constants
-#define INPUT_SIZE 256
-#define READ 0
-#define WRITE 1
+#define P_NUMBER 6
+#define C_NUMBER 4
+#define BUFFER_SIZE 12
+#define N_P_STEPS 4
+#define N_C_STEPS 6
+
+// Create buffer
+size_t widgets;
+// Create semaphores
+sem_t not_full, not_empty;
 
 int main() {
+	// Create pthreads
+	pthread_t	producers[P_NUMBER];
+	pthread_t consumers[C_NUMBER];
+	// Create buffer
+	size_t widgets;
+	// Create thread IDs
+	size_t tID[P_NUMBER + C_NUMBER];
+	for(size_t i = 1; i <= (P_NUMBER+C_NUMBER); i++)
+	{
+		tID[i-1] = i;
+	}
+	// Initialize semaphores
+	sem_init(&not_full, 0, (unsigned)BUFFER_SIZE);
+	sem_init(&not_empty, 0, (unsigned)0);
+
+	pthread_create(&producers[0], NULL, test_run, (void*) &tID[0]);
+	pthread_join(producers[0], NULL);
+	sem_destroy(&not_full);
+	sem_destroy(&not_empty);
+
   return 0;
 }
 /////////////////////////////////  Functions  //////////////////////////////////
@@ -28,3 +58,14 @@ Use:
 Arguments:
 Returns:
 *******************************************************************************/
+void* test_run(void* tID){
+	int *ID = (void*) tID;
+	printf("Hello World, this is thread: %d\n", *ID);
+	for(int i = 0; i < 10; i++)
+	{
+		sem_wait(&not_full);
+		widgets++;
+		sem_post(&not_empty);
+	}
+	return NULL;
+}
