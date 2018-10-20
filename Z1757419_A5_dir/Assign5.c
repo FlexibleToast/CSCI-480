039709d72e5be892b26deca731804ddb90f38eb6
Joseph McDade <JMcDade42@gmail.com>
2018-10-20 12:20:13 -0500

    Init Assignment 5

---
+++ Z1757419_A5_dir/Assign5.c
@@ -0,0 +1,30 @@
+/*******************************************************************************
+   PROGRAM:			CSCI 480 Assignment 5
+   PROGRAMMER:	Joseph McDade
+   LOGON ID:		z1757419
+   DUE DATE:		10/26/2018
+	 Note:				Uses hard tabs set to display as 2 spaces
+
+   FUNCTION:		Producer and Consumer problem using multiple producer and
+	 							consumer POSIX threads, semaphores, and mutexes.
+*******************************************************************************/
+
+#include <stdio.h>
+
+// Prototyping statements
+
+// Set global constants
+#define INPUT_SIZE 256
+#define READ 0
+#define WRITE 1
+
+int main() {
+  return 0;
+}
+/////////////////////////////////  Functions  //////////////////////////////////
+/*******************************************************************************
+Function:
+Use:
+Arguments:
+Returns:
+*******************************************************************************/
