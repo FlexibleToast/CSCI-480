737952a9f35fe342d24e29d8ab1baaad5f8451f1
Joseph McDade <JMcDade42@gmail.com>
2018-11-24 12:19:40 -0600

    Assign7: Initialize

---
+++ Z1757419_A7_dir/Assign7.h
@@ -0,0 +1,25 @@
+#ifndef ASSIGN7_H
+#define ASSIGN7_H
+/*******************************************************************************
+ * File:        Assign7.h
+ * Programmer:  Joseph McDade
+ * zID:         1757419
+ * Due Date:    11/30/2018
+ *
+ * Purpose:     Header file for Assign7 program
+ ******************************************************************************/
+#include <iostream>
+#include <iomanip>
+#include <list>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <string.h>
+
+// Prototyping statements
+
+// Set global constants
+
+// Create Queues
+
+#endif
---
+++ Z1757419_A7_dir/Entry.h
@@ -0,0 +1,32 @@
+#ifndef ENTRY_H
+#define ENTRY_H
+/*******************************************************************************
+ * File:        Entry.h
+ * Programmer:  Joseph McDade
+ * zID:         1757419
+ * Due Date:    11/30/2018
+ *
+ * Purpose:     Header file for Entry object class
+ ******************************************************************************/
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+class Entry
+{
+  public:
+		//Constructors
+    Process();
+
+    //Accessor Methods
+		//Get Methods
+		//Set Methods
+
+    //Utility Methods
+
+		//Operator Overload
+
+};
+
+#endif
